40894579771cbdc9a634608d02d635a0e7c35867
Ariq Khoiri <qq.khoiri@gmail.com>
2023-05-12 13:35:02 +0700

    easy:palindrome number

---
+++ src/test/java/com/neko/levelupmyskillcoodinginterview/PalindromeNumber.java
@@ -0,0 +1,33 @@
+package com.neko.levelupmyskillcoodinginterview;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PalindromeNumber {
+
+    @Test
+    void testLeetCode9() {
+        /**
+         * Given an integer x, return true if x is a
+         * palindrome
+         * ,and false otherwise.
+         *
+         * Input: x = -121
+         * Output: false
+         * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+         */
+        int x = 121;
+        String xs = String.valueOf(x);
+
+        boolean status = true;
+        for (int i = 0; i < xs.length(); i++) {
+            String currentValue = String.valueOf(xs.charAt(i));
+
+            if (!currentValue.equals(String.valueOf(xs.charAt(xs.length()-1-i)))) {
+                status = false;
+            }
+        }
+
+        Assertions.assertTrue(status);
+    }
+}
