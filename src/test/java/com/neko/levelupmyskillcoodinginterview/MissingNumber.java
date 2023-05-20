789a92da8c16781fa7b6b3c7feceaa0ec32590c8
Ariq Khoiri <qq.khoiri@gmail.com>
2023-05-20 16:42:22 +0700

    easy:missing value : but not ok

---
+++ src/test/java/com/neko/levelupmyskillcoodinginterview/MissingNumber.java
@@ -0,0 +1,43 @@
+package com.neko.levelupmyskillcoodinginterview;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    @Test
+    void testMissingNumber() {
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+        System.out.println(missingNumber(nums));
+    }
+
+    public int missingNumber(int[] nums) {
+        for (int i = 0; i <= nums.length; i++) {
+            if (!binarySearch(nums, i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean binarySearch(int []nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+
+        Arrays.sort(nums);
+        while(high >= low) {
+            int mid = (low + high) / 2;
+
+            if (target == nums[mid]) {
+                return true;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
